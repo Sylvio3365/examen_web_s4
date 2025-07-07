3bc01ad85515c5e5f5b423d386d4f30eab5e9bea
Miojoro RAHAJANIAINA <miojoropro@hotmail.com>
2025-07-07 19:06:40 +0300

    mis à jour type prêt
    nouvelle table

--- ws/models/TypePret.php
+++ ws/models/TypePret.php
@@ -4,33 +4,46 @@ require_once __DIR__ . '/../db.php';
 class TypePret {
     public static function getAll() {
         $db = getDB();
-        $stmt = $db->query("SELECT * FROM type_pret");
+        $stmt = $db->query("SELECT * FROM typepret WHERE deleted_at IS NULL");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public static function getById($id) {
         $db = getDB();
-        $stmt = $db->prepare("SELECT * FROM type_pret WHERE id = ?");
+        $stmt = $db->prepare("SELECT * FROM typepret WHERE idtypepret = ? AND deleted_at IS NULL");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public static function create($data) {
         $db = getDB();
-        $stmt = $db->prepare("INSERT INTO type_pret (nom, taux_interet, duree_mois) VALUES (?, ?, ?)");
-        $stmt->execute([$data->nom, $data->taux_interet, $data->duree_mois]);
+        $stmt = $db->prepare("INSERT INTO typepret (nom, taux_annuel, montant_min, montant_max, duree_max) VALUES (?, ?, ?, ?, ?)");
+        $stmt->execute([
+            $data->nom, 
+            $data->taux_annuel, 
+            $data->montant_min, 
+            $data->montant_max, 
+            $data->duree_max
+        ]);
         return $db->lastInsertId();
     }
 
     public static function update($id, $data) {
         $db = getDB();
-        $stmt = $db->prepare("UPDATE type_pret SET nom = ?, taux_interet = ?, duree_mois = ? WHERE id = ?");
-        $stmt->execute([$data->nom, $data->taux_interet, $data->duree_mois, $id]);
+        $stmt = $db->prepare("UPDATE typepret SET nom = ?, taux_annuel = ?, montant_min = ?, montant_max = ?, duree_max = ? WHERE idtypepret = ? AND deleted_at IS NULL");
+        $stmt->execute([
+            $data->nom, 
+            $data->taux_annuel, 
+            $data->montant_min, 
+            $data->montant_max, 
+            $data->duree_max, 
+            $id
+        ]);
     }
 
     public static function delete($id) {
         $db = getDB();
-        $stmt = $db->prepare("DELETE FROM type_pret WHERE id = ?");
+        $stmt = $db->prepare("UPDATE typepret SET deleted_at = CURRENT_DATE() WHERE idtypepret = ?");
         $stmt->execute([$id]);
     }
 }
