1ad58462e9c24df461eab02ba123c2866979895f
Miojoro RAHAJANIAINA <miojoropro@hotmail.com>
2025-07-08 01:00:21 +0300

    Debug

--- ws/controllers/PretController.php
+++ ws/controllers/PretController.php
@@ -25,23 +25,6 @@ class PretController
         }
         
         Flight::json([
-    public function interets()
-    {
-        $pretModel = new Pret();
-
-        // Valeurs par défaut (derniers 12 mois)
-        $dateDebut = date('Y-m-d', strtotime('-12 months'));
-        $dateFin = date('Y-m-d');
-
-        // Si le formulaire est soumis
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $dateDebut = $_POST['date_debut'] ?? $dateDebut;
-            $dateFin = $_POST['date_fin'] ?? $dateFin;
-        }
-
-        $interets = $pretModel->getInteretsParPeriode($dateDebut, $dateFin);
-
-        Flight::render('interets', [
             'interets' => $interets,
             'dateDebut' => $dateDebut,
             'dateFin' => $dateFin
